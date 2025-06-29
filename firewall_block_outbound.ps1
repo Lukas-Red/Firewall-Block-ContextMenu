43b79bb22f80affc38bba11330bdb285e34ba5a4
Lukas Roncalli <46164050+Lukas-Red@users.noreply.github.com>
2025-06-29 20:49:04 +0200

    Add files via upload

---
+++ firewall_block_outbound.ps1
@@ -0,0 +1,10 @@
+﻿param(
+    [string]$FilePath
+)
+
+
+$fileName = $FilePath.Split("\\")[-1]
+$Date = Get-Date
+$DateStr = $Date.ToString("yyyy-MM-dd")
+
+New-NetFirewallRule -DisplayName ("SCRIPT " + $DateStr + " Block " + $fileName) -Description "Rule generated with the block outbound script" -Direction Outbound -Action Block -Program $FilePath
---
+++ firewall_block_outbound_elevator.ps1
@@ -0,0 +1,5 @@
+﻿param(
+    [string]$FilePath
+)
+
+Start-Process powershell -Verb runAs -ArgumentList 'C:\path\to\firewall_block_outbound.ps1',$FilePath -WindowStyle hidden
